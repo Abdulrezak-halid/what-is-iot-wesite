e814b38a733312e0b04764c83a548b945c64cd66
Abdulrezak Halit <abdulrezak.khaled@gmail.com>
2026-02-12 22:59:49 +0300

    chore: initial commit :)

---
+++ menu-list.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".humbarger").click(function (event) {
+    $(".menu-list").slideToggle(500);
+    event.preventDefault();
+
+    $(".menu-list li a").click(function (event) {
+      if ($(window).width() < 768) {
+        $(".menu-list").slideUp(500);
+        event.preventDefault();
+      }
+    });
+  });
+});
